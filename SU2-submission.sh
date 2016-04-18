51c537f07859318041fc80078e8eb8321cc3be0d
HummingTree <ducknade@gmail.com>
2016-04-18 19:33:42 -0400

    Adding PBS file.

---
+++ SU2-submission.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+### specify the queue and that job is not re-runable,
+### as well as the resources and the name
+#PBS -q cuth
+#PBS -r n
+#PBS -l nodes=1,walltime=200:00:00
+#PBS -N SU2
+#PBS -o /home/jt2798/cuth-log
+#PBS -e /home/jt2798/cuth-log
+
+date
+
+/home/jt2798/SU2Gauge/SU2Gauge
+
+date
+
